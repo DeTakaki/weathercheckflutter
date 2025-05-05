3605c6d3103370874492f945c699d97631299785
Diego Takaki <diego.matsubara@gmail.com>
2025-05-05 12:06:30 -0400

    feat: Adjusting flutter version within vscode specs

--- weather_check_app/test/widget_test.dart
+++ weather_check_app/test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:weather_check_app/main.dart';
+import 'package:weather_checking/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
